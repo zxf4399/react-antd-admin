16d9f35355d0a0ec20cfa8a053f4e828d4c5ba85
zxf4399 <zxf4399@gmail.com>
2022-05-04 18:24:26 +0800

    feat: remove service-worker import

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,7 @@ import "@/styles/base.css";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 
-import { isProd } from "@/utils/const";
-
 import App from "./App";
-
-if (isProd && "serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/tech-stack/service-worker.js");
-  });
-}
 
 const root = createRoot(document.getElementById("root") as HTMLDivElement);
 
